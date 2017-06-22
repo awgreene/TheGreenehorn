3df840fb98ce195cb3a52daac30f5b09a2957900
Alexander W. Greene <awgreene@Alexanders-MacBook-Pro.local>
2017-06-22 08:48:08 -0400

    Add home page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get("/", function(req, res) {
     res.render("landing");
 });
 
+// ROOT ROUTE
+router.get("/home", function(req, res) {
+    res.render("home");
+});
+
 module.exports = router;
