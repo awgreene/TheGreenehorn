801d4b105ae3f2d15ac0fd36dc91c3628154010c
Alexander W. Greene <awgreene@Alexanders-MacBook-Pro.local>
2017-06-29 07:59:31 -0400

    Add local copy of fontawesome and bootstrap, update color scheme, add first phase of blog page.

--- routes/blogs.js
+++ routes/blogs.js
@@ -8,7 +8,34 @@ router.get("/", function(req, res) {
     if(err) {
       console.log(err);
     } else {
-      res.render("blogs", {blogs: blogs});
+      res.render("blogs/index", {blogs: blogs});
+    }
+  });
+});
+
+// NEW ROUTE
+router.get("/new",function(req,res){
+  res.render("blogs/new");
+});
+
+// CREATE ROUTE
+router.post("/", function(req, res){
+  Blog.create(req.body.blog, function(err, blog) {
+    if(err){
+      res.render("blogs/new");
+    } else {
+      res.redirect("/blogs");
+    }
+  });
+});
+
+// SHOW ROUTE
+router.get("/:id", function(req, res) {
+  Blog.findById(req.params.id, function(err, foundBlog) {
+    if(err){
+      res.redirect("/");
+    } else {
+      res.render("blogs/show", {blog:foundBlog});
     }
   });
 });
