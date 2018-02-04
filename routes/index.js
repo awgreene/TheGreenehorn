72abeaddf06f9bf2843e8222d53d8cd526de69e4
Alexander Greene <awgreene@us.ibm.com>
2018-02-04 08:42:50 -0500

    Rebranding

--- routes/index.js
+++ routes/index.js
@@ -3,22 +3,12 @@ var router = express.Router();
 
 // LANDING ROUTE
 router.get("/", function(req, res) {
-  res.render("landing");
-});
-
-// HOME ROUTE
-router.get("/home", function(req, res) {
   res.render("home");
 });
 
-// ABOUT ROUTE
-router.get("/about", function(req, res) {
-  res.render("about");
-});
-
-// CONTACT ROUTE
-router.get("/contact", function(req, res) {
-  res.render("contact");
+// WORK ROUTE
+router.get("/portfolio", function(req, res) {
+  res.render("portfolio");
 });
 
 // Blogs ROUTE
